530bdb02de3cd640fe69d650105f08b782b6267b
Zurab Dzindzibadze <dzindzibadzezurabi@gmail.com>
2021-08-08 17:00:49 +0400

    runtime python version detection

---
+++ PythonicThinking/python_version.py
@@ -0,0 +1,7 @@
+# Finding python version at runtime with additionl information
+import sys
+
+print(sys.version_info)
+print(sys.version)
+
+# Always Enforce pep8 unless you follow other guidline for some reason
